b1af292be5b25a8ca535cc73586fd8216877df98
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-12-29 01:24:13 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+// const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
