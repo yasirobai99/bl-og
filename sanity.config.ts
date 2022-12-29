8f18beddd8706031fe56fdc42632bf7df2fe0c7e
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-12-30 00:36:22 +0530

    First Commit

---
+++ sanity.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'sanity';
+import {deskTool} from 'sanity/desk';
+import {visionTool} from '@sanity/vision';
+import {schemaTypes} from './schemas';
+import { myTheme } from './theme';
+import StudioNavbar from './components/StudioNavbar';
+import Logo from './components/Logo';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio",
+  name: 'Yasir_bl-og',
+  title: 'bl-og',
+projectId,
+  dataset,
+plugins: [deskTool(), visionTool()],
+schema: {
+    types: schemaTypes,
+  },
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar
+    }
+  },
+  theme: myTheme,
+});
