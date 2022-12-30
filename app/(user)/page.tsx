6a7abfbbbc85faf4ed25a1d1e58f896f6301d155
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-12-31 00:23:35 +0530

    First Commit

---
+++ app/(user)/page.tsx
@@ -0,0 +1,35 @@
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
+
+const query = groq`
+*[_type=='post'] {
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt desc)
+`;
+
+export default async function HomePage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense 
+      fallback={
+        <div role="status">
+          <p className=" text-center text-lg animate-pulse text-[#00de7a]">
+            Loading Priview Data...
+          </p>
+        </div>
+      }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    ); 
+  }
+
+  const posts = await client.fetch(query);
+return <BlogList posts={posts} />
+}
