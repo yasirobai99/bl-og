62c72b252cb876cbf18f265a086887b365ba2187
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-12-31 22:41:09 +0530

    First Commit

--- components/PreviewBlogList.tsx
+++ components/PreviewBlogList.tsx
@@ -6,7 +6,6 @@ import BlogList from "./BlogList";
 type Props ={
     query: string;
 }
-
 export default function PreviewBlogList({ query }:Props){
 const posts = usePreview(null, query);
 console.log("LOADING posts...", posts);
