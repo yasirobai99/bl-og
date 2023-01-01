ca73769d024cba25a24e6ac17d7c0eb06093de19
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2023-01-01 18:36:35 +0530

    Final Commit

--- typings.d.ts
+++ typings.d.ts
@@ -1,71 +1,69 @@
-// import { AuthStoreOptions, Image, Reference, Slug } from "sanity";
-
-type Base ={
-    _cretedAt: string;
-    _id: string;
-    _rev: string;
-    _type: string;
-    _updatedAt: string;
+type Base = {
+	_createdAt: string;
+	_id: string;
+	_rev: string;
+	_type: string;
+	_updatedAt: string;
 };
 
 interface Post extends Base {
-    author: Author;
-    body: Block[];
-    catagories: Category[];
-    mainImage: Image;
-    slug: Slug;
-    title: string;
-    description: string;
+	author: Author;
+	body: Block[];
+	categories: Category[];
+	mainImage: Image;
+	slug: Slug;
+	title: string;
+	description: string;
 }
 
 interface Author extends Base {
-    bio: Block[];
-    image: Image;
-    name: string;
-    slug: Slug;
+	bio: Block[];
+	image: Image;
+	name: string;
+	slug: Slug;
 }
 
 interface Image {
-    _type: "image";
-    asset: Reference;
+	_type: "image";
+	asset: Reference;
 }
 
 interface Reference {
-    _ref: string;
-    _type: "reference";
+	_ref: string;
+	_type: "reference";
 }
 
 interface Slug {
-    _type: "slug";
-    current: string;
+	_type: "slug";
+	current: string;
 }
 
 interface Block {
-    _key: string;
-    _type: "block";
-    children: Span[];
-    markDefs: any[];
-    style: "normal" | "h1" | "h2" | "h3"| "h4" | "blockquote";
+	_key: string;
+	_type: "block";
+	children: Span[];
+	markDefs: any[];
+	style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
 }
 
 interface Span {
-    _key: string;
-    _type: "span";
-    marks: string[];
-    text: string;
+	_key: string;
+	_type: "span";
+	marks: string[];
+	text: string;
 }
 
 interface Category extends Base {
-    description: string;
-    title: string;
+	title: string;
+	description: string;
 }
 
 interface MainImage {
-_type: "image";
-asset: Reference;
+	_type: "image";
+	asset: Reference;
 }
 
 interface Title {
-    _type: "string";
-    current: string;
+	_type: "string";
+	current: string;
 }
